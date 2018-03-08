4b6fe6b95918d996227da92f88ef8066cd60259e
Rhys Powell <rhys@rpowell.me>
2018-03-08 18:45:54 +1100

    DRY out story feed screens

--- src/components/HottestStoriesScreen.js
+++ src/components/HottestStoriesScreen.js
@@ -4,28 +4,18 @@
  * @flow
  */
 
-import React, { Component } from 'react'
-import { StyleSheet, View, StatusBar, Linking, Platform } from 'react-native'
-import SafariView from 'react-native-safari-view'
+import React, { PureComponent } from 'react'
 import { standardNavigationOptions } from '../lib/navigation'
-import { accentColor, accentDarkColor } from '../lib/colors'
 import { fetchHottestStories } from '../api'
-import StoryFeed from './StoryFeed'
+import StoryFeedScreen from './StoryFeedScreen'
 
-import type { StorySummary } from '../api'
 import type { NavigationNavigatorProps } from 'react-navigation'
 
 type NavigationState = {}
 
 type Props = {} & NavigationNavigatorProps<{}, NavigationState>
 
-type State = {
-  stories: StorySummary[],
-  isLoading: boolean,
-  loadingError: ?Error
-}
-
-export default class HottestStoriesScreen extends Component<Props, State> {
+export default class HottestStoriesScreen extends PureComponent<Props> {
   static navigationOptions = ({
     navigation
   }: NavigationNavigatorProps<{}, NavigationState>) => ({
@@ -35,78 +25,12 @@ export default class HottestStoriesScreen extends Component<Props, State> {
     title: 'Top Stories'
   })
 
-  state = {
-    stories: [],
-    isLoading: false,
-    loadingError: null
-  }
-
-  componentDidMount() {
-    if (!this.state.loading) {
-      this.fetchStories()
-    }
-  }
-
-  fetchStories = () => {
-    this.setState({ isLoading: true, loadingError: null }, async () => {
-      try {
-        const stories = await fetchHottestStories()
-        this.setState({ stories, isLoading: false })
-      } catch (e) {
-        this.setState({ loadingError: e, isLoading: false })
-      }
-    })
-  }
-
-  handleSelectStory = (story: StorySummary) => {
-    if (story.url == null || story.url.length === 0) {
-      this.handleSelectStoryComments(story)
-      return
-    }
-
-    if (Platform.OS === 'ios' && SafariView.isAvailable()) {
-      SafariView.show({
-        url: story.url,
-        tintColor: accentColor,
-        fromBottom: true
-      })
-    } else if (Linking.canOpenURL(story.url)) {
-      Linking.openURL(story.url)
-    } else {
-      console.error(`Unable to open link: ${story.url}`)
-    }
-  }
-
-  handleSelectStoryComments = (story: StorySummary) => {
-    this.props.navigation.navigate('Story', {
-      id: story.short_id,
-      story: story
-    })
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar
-          backgroundColor={accentDarkColor}
-          networkActivityIndicatorVisible={this.state.isLoading}
-        />
-        <StoryFeed
-          stories={this.state.stories}
-          isLoading={this.state.isLoading}
-          onSelectStory={this.handleSelectStory}
-          onSelectStoryComments={this.handleSelectStoryComments}
-          onRefresh={this.fetchStories}
-        />
-      </View>
+      <StoryFeedScreen
+        fetchStories={fetchHottestStories}
+        navigation={this.props.navigation}
+      />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
--- src/components/NewestStoriesScreen.js
+++ src/components/NewestStoriesScreen.js
@@ -4,28 +4,18 @@
  * @flow
  */
 
-import React, { Component } from 'react'
-import { StyleSheet, View, StatusBar, Linking, Platform } from 'react-native'
-import SafariView from 'react-native-safari-view'
+import React, { PureComponent } from 'react'
 import { standardNavigationOptions } from '../lib/navigation'
-import { accentColor, accentDarkColor } from '../lib/colors'
 import { fetchNewestStories } from '../api'
-import StoryFeed from './StoryFeed'
+import StoryFeedScreen from './StoryFeedScreen'
 
-import type { StorySummary } from '../api'
 import type { NavigationNavigatorProps } from 'react-navigation'
 
 type NavigationState = {}
 
 type Props = {} & NavigationNavigatorProps<{}, NavigationState>
 
-type State = {
-  stories: StorySummary[],
-  isLoading: boolean,
-  loadingError: ?Error
-}
-
-export default class NewestStoriesScreen extends Component<Props, State> {
+export default class NewestStoriesScreen extends PureComponent<Props> {
   static navigationOptions = ({
     navigation
   }: NavigationNavigatorProps<{}, NavigationState>) => ({
@@ -35,78 +25,12 @@ export default class NewestStoriesScreen extends Component<Props, State> {
     title: 'Recent'
   })
 
-  state = {
-    stories: [],
-    isLoading: false,
-    loadingError: null
-  }
-
-  componentDidMount() {
-    if (!this.state.loading) {
-      this.fetchStories()
-    }
-  }
-
-  fetchStories = () => {
-    this.setState({ isLoading: true, loadingError: null }, async () => {
-      try {
-        const stories = await fetchNewestStories()
-        this.setState({ stories, isLoading: false })
-      } catch (e) {
-        this.setState({ loadingError: e, isLoading: false })
-      }
-    })
-  }
-
-  handleSelectStory = (story: StorySummary) => {
-    if (story.url == null || story.url.length === 0) {
-      this.handleSelectStoryComments(story)
-      return
-    }
-
-    if (Platform.OS === 'ios' && SafariView.isAvailable()) {
-      SafariView.show({
-        url: story.url,
-        tintColor: accentColor,
-        fromBottom: true
-      })
-    } else if (Linking.canOpenURL(story.url)) {
-      Linking.openURL(story.url)
-    } else {
-      console.error(`Unable to open link: ${story.url}`)
-    }
-  }
-
-  handleSelectStoryComments = (story: StorySummary) => {
-    this.props.navigation.navigate('Story', {
-      id: story.short_id,
-      story: story
-    })
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar
-          backgroundColor={accentDarkColor}
-          networkActivityIndicatorVisible={this.state.isLoading}
-        />
-        <StoryFeed
-          stories={this.state.stories}
-          isLoading={this.state.isLoading}
-          onSelectStory={this.handleSelectStory}
-          onSelectStoryComments={this.handleSelectStoryComments}
-          onRefresh={this.fetchStories}
-        />
-      </View>
+      <StoryFeedScreen
+        fetchStories={fetchNewestStories}
+        navigation={this.props.navigation}
+      />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
